0c0817f929b1b5d4238814e0ccdc23f80adc8334
asandamhlongo <156101659+asandamhlongo@users.noreply.github.com>
2024-06-13 23:05:49 +0200

    Created a gallery section and created an image slider for it. I styled it on css and added js code for functionality

--- index.js
+++ index.js
@@ -22,4 +22,23 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(box);
     });
 });
+/*
+const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    }
+    // Update scrollbar thumb position based on image scroll
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    }
+    // Call these two functions when image list scrolls
+    imageList.addEventListener("scroll", () => {
+        updateScrollThumbPosition();
+        handleSlideButtons();
+    });
 
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
+*/
